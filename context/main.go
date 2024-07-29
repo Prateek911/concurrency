9d63f290ddba03278fb9aac3c610cdbec65fb4c8
Prateek911 <prateek.work911@gmail.com>
2024-07-29 20:28:28 +0530

    added waitGroup and Context

---
+++ context/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	var urls = []string{"http://www.golang.org/", "http://www.google.com/", "https://overgear.com/games/wow/raids"}
+
+	results := make(chan string)
+
+	for _, url := range urls {
+		go fetchAPI(ctx, url, results)
+	}
+
+	for range urls {
+		fmt.Println(<-results)
+	}
+
+}
+
+func fetchAPI(ctx context.Context, url string, results chan<- string) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		results <- fmt.Sprintf("Error creating request for %s: %s", url, err.Error())
+		return
+	}
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		results <- fmt.Sprintf("Error making request to %s: %s", url, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	results <- fmt.Sprintf("Response from %s: %d", url, resp.StatusCode)
+
+}
